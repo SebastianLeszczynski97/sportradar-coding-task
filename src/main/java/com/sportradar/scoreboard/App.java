3f786f63c475b2bf76d758545356cb00b5aa3126
Sebastian Leszczyński <sebastian.leszcz@gmail.com>
2025-07-03 00:54:23 +0200

    feat: add interactive option to finish game

--- src/main/java/com/sportradar/scoreboard/App.java
+++ src/main/java/com/sportradar/scoreboard/App.java
@@ -1,24 +1,63 @@
 package com.sportradar.scoreboard;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
         ScoreBoard scoreBoard = new ScoreBoard();
-        Game firstGame = new Game("Poland","England",0,0);
-        Game secondGame = new Game("Germany","France",0,0);
-        Game thirdGame = new Game("Spain","Italy",0,0);
+        Game firstGame = new Game("Poland", "England", 0, 0);
+        Game secondGame = new Game("Germany", "France", 0, 0);
+        Game thirdGame = new Game("Spain", "Italy", 0, 0);
 
         scoreBoard.addGame(firstGame);
         scoreBoard.addGame(secondGame);
         scoreBoard.addGame(thirdGame);
 
-        String results = scoreBoard.getCurrentResults();
+        scoreBoard.updateGame(new Game("Poland", "England", 1, 0));
 
-        System.out.println(results);
+        displayScoreBoard(scoreBoard);
+        displayOptions();
+
+        Scanner input = new Scanner(System.in);
+        int option = input.nextInt();
+
+        while (option != 9) {
+            switch (option) {
+                case 1:
+                    System.out.println("Please enter home team");
+                    String homeTeam = input.next();
+                    System.out.println("Please enter away team");
+                    String awayTeam = input.next();
+                    scoreBoard.finishGame(homeTeam, awayTeam);
+                    break;
+
+                case 2:
+                    //Update game logic
+                    break;
+                case 3:
+                    //Add game logic
+                    break;
+            }
+            displayScoreBoard(scoreBoard);
+            displayOptions();
+
+            option = input.nextInt(); // add this
+        }
+    }
+
+    public static void displayOptions() {
+        System.out.println("To finish game press 1");
+        System.out.println("To update game press 2");
+        System.out.println("To close the application game press 9");
+    }
+
+    public static void displayScoreBoard(ScoreBoard scoreBoard) {
+        System.out.println("Updated score: \n");
+        System.out.println("--------------------------");
+        System.out.println(scoreBoard.getCurrentResults());
+        System.out.println("--------------------------");
     }
 }
