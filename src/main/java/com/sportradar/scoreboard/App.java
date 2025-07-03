c2f5f82c85ddf92c848149a972ffe25d104ca573
Sebastian Leszczyński <sebastian.leszcz@gmail.com>
2025-07-03 19:16:08 +0200

    refactor: extract smaller methods out of main application logic

--- src/main/java/com/sportradar/scoreboard/App.java
+++ src/main/java/com/sportradar/scoreboard/App.java
@@ -6,27 +6,31 @@ import java.util.Scanner;
  * Hello world!
  */
 public class App {
+    public static final int FINISH_GAME_OPTION = 1;
+    public static final int UPDATE_GAME_OPTION = 2;
+    public static final int ADD_GAME_OPTION = 3;
+    public static final int EXIT_APPLICATION_OPTION = 9;
+
+
     public static void main(String[] args) {
         ScoreBoard scoreBoard = new ScoreBoard();
-        Game firstGame = new Game("Poland", "England");
-        Game secondGame = new Game("Germany", "France");
-        Game thirdGame = new Game("Spain", "Italy");
 
-        scoreBoard.addGame(firstGame);
-        scoreBoard.addGame(secondGame);
-        scoreBoard.addGame(thirdGame);
-
-        scoreBoard.updateGame(new Game("Poland", "England", 1, 0));
+        createExampleGames(scoreBoard);
+        updateExampleGame(scoreBoard);
 
         displayScoreBoard(scoreBoard);
         displayOptions();
 
+        handleUserInput(scoreBoard);
+    }
+
+    private static void handleUserInput(ScoreBoard scoreBoard) {
         Scanner input = new Scanner(System.in);
         int option = input.nextInt();
 
-        while (option != 9) {
+        while (option != EXIT_APPLICATION_OPTION) {
             switch (option) {
-                case 1:
+                case FINISH_GAME_OPTION:
                     System.out.println("Please enter home team");
                     String homeTeam = input.next();
                     System.out.println("Please enter away team");
@@ -34,7 +38,7 @@ public class App {
                     scoreBoard.finishGame(homeTeam, awayTeam);
                     break;
 
-                case 2:
+                case UPDATE_GAME_OPTION:
                     System.out.println("Please enter home team");
                     String updatedHomeTeam = input.next();
                     System.out.println("Please enter away team");
@@ -49,7 +53,7 @@ public class App {
                         System.out.println(exception.getMessage());
                     }
                     break;
-                case 3:
+                case ADD_GAME_OPTION:
                     System.out.println("Please enter home team");
                     String newHomeTeam = input.next();
                     System.out.println("Please enter away team");
@@ -64,17 +68,32 @@ public class App {
         }
     }
 
-    public static void displayOptions() {
+    private static void displayOptions() {
         System.out.println("To finish game press 1");
         System.out.println("To update game press 2");
         System.out.println("To add new game press 3");
         System.out.println("To close the application game press 9");
     }
 
-    public static void displayScoreBoard(ScoreBoard scoreBoard) {
+    private static void displayScoreBoard(ScoreBoard scoreBoard) {
         System.out.println("Current score: ");
         System.out.println("--------------------------");
         System.out.println(scoreBoard.getCurrentResults());
         System.out.println("--------------------------");
+    }
+
+    private static void createExampleGames(ScoreBoard scoreBoard) {
+        Game firstGame = new Game("Poland", "England");
+        Game secondGame = new Game("Germany", "France");
+        Game thirdGame = new Game("Spain", "Italy");
+
+        scoreBoard.addGame(firstGame);
+        scoreBoard.addGame(secondGame);
+        scoreBoard.addGame(thirdGame);
+    }
+
+    private static void updateExampleGame(ScoreBoard scoreBoard) {
+        Game exampleGame = new Game("Poland", "England", 1, 0);
+        scoreBoard.updateGame(exampleGame);
     }
 }
